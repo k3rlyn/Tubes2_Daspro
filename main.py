435ed5884bd88b26c47b0a5a5e23a2fe0cab05eb
truk <130665228+jonathanw33@users.noreply.github.com>
2023-04-14 19:34:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from terlarang import *
+from fungsi_login import *
+from test import *
 
+users = read_csv_file('data_csv\\user.csv', separator=';')
+candi = read_csv_file('data_csv\\candi.csv', separator=';')
+#bahan_bangunan = read_csv_file('data_csv\\bahan_bangunan.csv', separator=';')
+
+login_status = False
+username = ""
+
+while True:
+    masukan = input(">>> ")
+    if masukan == "login":
+        username,login_status = login(users=users,login_status=login_status,username=username)
+    else : 
+      ("test awal")
