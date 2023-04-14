2502d9d7bdb8cebf525d1f59184d8bdbc603143e
truk <130665228+jonathanw33@users.noreply.github.com>
2023-04-14 11:43:17 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
 from terlarang import *
 
-length("fjaskfjas")
+def login():
+    username = input("Username: ")
+    password = input("Password: ")
+    arr = split_manual("user.csv")
+    login_status = False
+    user = ""
+    for i in range(len(arr)):
+        if username == arr[i][0]:
+            if password == arr[i][1]:
+                print("Selamat datang,", arr[i][0])
+                login_status = True
+                user = arr[i][0]
+                return user, login_status
+            else:
+                print("Password salah!")
+                return user, login_status
+        else:
+            print("Username tidak terdaftar!")
+            return user, login_status
